ba5a1146657d587e3bd5a6b0405d5f1aa716bc78
Oleg Mironov <oleg.mironov@eficode.com>
2017-03-30 15:12:46 +0300

    Fix credentials classname

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -29,7 +29,7 @@ def pushDockerImages(tag) {
 }
 
 def upgradeEnvironment(environment) {
-    withCredentials([[$class: 'AmazonWebServicesCredentialsMultiBinding', credentialsId: '15736741-7880-434d-83b8-9c255ba8cd60',
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: '15736741-7880-434d-83b8-9c255ba8cd60',
                             accessKeyVariable: 'ACCESSKEY', secretKeyVariable: 'SECRETKEY']]) 
     {
         if (environment == 'development') {
